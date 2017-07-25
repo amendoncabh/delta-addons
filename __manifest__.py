3b14494aa3a94937ef8ce2a927332859bb43f726
rodriguesraphael <raphael.rodrigues@gobtelecom.com.br>
2017-07-24 23:59:39 -0300

    Projeto para emissão de NFCe no Odoo

---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Emissão de NFC-e",
+    'summary': """
+           Permite a emissão de NFC-e através das Vendas no Odoo
+           """,
+    'description': """
+        Permite a emissão de NFC-e através das Vendas no Odoo
+    """,
+    'author': "Raphael Rodrigues",
+    'website': "",
+    'category': 'account',
+    'version': '0.0.1.0.0',
+    'depends': ['base',
+                'br_nfe'],
+    
+    'external_dependencies': {
+        'python': [
+            'pytrustnfe', 'pytrustnfe.nfe',
+            'pytrustnfe.certificado', 'pytrustnfe.utils'
+        ],
+    },
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'aplication': True,
+}
